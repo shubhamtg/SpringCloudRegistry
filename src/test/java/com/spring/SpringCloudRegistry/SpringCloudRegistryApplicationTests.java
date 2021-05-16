853251e8e3504319bd48de6a09c8562860384274
shubhamtg <shubhamtg18@gmail.com>
2021-05-16 12:54:38 +0530

    Initial commit

---
+++ src/test/java/com/spring/SpringCloudRegistry/SpringCloudRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.SpringCloudRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
